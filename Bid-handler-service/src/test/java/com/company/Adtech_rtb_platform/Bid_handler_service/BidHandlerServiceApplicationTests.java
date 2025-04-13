004075f47aa26f5a0af0366137c00e6883dc80c8
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-13 10:06:00 +0530

    added the bid-handler-service

---
+++ Bid-handler-service/src/test/java/com/company/Adtech_rtb_platform/Bid_handler_service/BidHandlerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Adtech_rtb_platform.Bid_handler_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidHandlerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
