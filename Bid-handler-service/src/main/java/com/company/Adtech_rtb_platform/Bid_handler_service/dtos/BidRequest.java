63a01060c7932ddde790af787c51ecca5c6cb629
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-13 10:41:34 +0530

    added the dtos

---
+++ Bid-handler-service/src/main/java/com/company/Adtech_rtb_platform/Bid_handler_service/dtos/BidRequest.java
@@ -0,0 +1,11 @@
+package com.company.Adtech_rtb_platform.Bid_handler_service.dtos;
+
+
+import lombok.Data;
+
+@Data
+public class BidRequest {
+
+    private Double amount;
+    private String bidderId;
+}
