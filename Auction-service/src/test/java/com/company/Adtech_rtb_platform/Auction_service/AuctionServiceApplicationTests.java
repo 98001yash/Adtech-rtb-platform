b7543763e7eac8f50fe5aae3361116e938db0fc4
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-13 11:39:39 +0530

    Added the Auction-service

---
+++ Auction-service/src/test/java/com/company/Adtech_rtb_platform/Auction_service/AuctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Adtech_rtb_platform.Auction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
