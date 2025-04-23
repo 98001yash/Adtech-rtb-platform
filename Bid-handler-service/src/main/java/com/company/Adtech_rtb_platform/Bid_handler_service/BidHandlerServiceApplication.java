8ad5aa77b3cb4b60fdf1f8be7196c5dd56776077
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-23 07:42:41 +0530

    added the feign client configuration

--- Bid-handler-service/src/main/java/com/company/Adtech_rtb_platform/Bid_handler_service/BidHandlerServiceApplication.java
+++ Bid-handler-service/src/main/java/com/company/Adtech_rtb_platform/Bid_handler_service/BidHandlerServiceApplication.java
@@ -2,8 +2,11 @@ package com.company.Adtech_rtb_platform.Bid_handler_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BidHandlerServiceApplication {
 
 	public static void main(String[] args) {
